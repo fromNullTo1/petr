9f50cf4986525f579babeb639dfd72a776f5382e
Wadzim <wadzim.dev@gmail.com>
2022-08-26 15:57:18 +0300

    feat: add mobile events

---
+++ 34 mobile events/script.js
@@ -0,0 +1,39 @@
+'use script';
+
+// touchstart
+// touchmove
+// touchend
+// touchenter
+// touchleave
+// touchcancel
+
+window.addEventListener('DOMContentLoaded', () => {
+  const box = document.querySelector('.box');
+
+  box.addEventListener('touchstart', e => {
+    e.preventDefault();
+
+    console.log('start');
+    console.log(e.touches);
+    console.log(e.targetTouches); 
+    console.log(e.changeTouches);
+  });
+  
+  box.addEventListener('touchmove', e => {
+    e.preventDefault();
+
+    console.log('move');
+    console.log(e.targetTouches[0].pageX);
+  });
+  box.addEventListener('touchend', e => {
+    e.preventDefault();
+
+    console.log('end');
+  });
+
+})
+
+// touches
+
+// console.log(e.targetTouches); 
+// console.log(e.changeTouches);
